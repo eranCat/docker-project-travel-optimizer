b76d5e20486732ceb733028005ba8be1771f64db
Eran <30321396+eranCat@users.noreply.github.com>
2025-08-02 03:07:08 -0700

    remove log from pois post

--- maps_service/app/main.py
+++ maps_service/app/main.py
@@ -35,7 +35,6 @@ async def pois(request: RouteGenerationRequest):
     Given interests, location, radius, num_routes etc. return a list of POIs.
     """
     logging.info("🧭 /pois/ endpoint hit")
-    logging.info("METHOD HITTING /pois/:", request.method)
     tags = get_overpass_tags_from_interests(request.interests)
     logging.debug(f"Generated tags from interests: {tags}")
     pois = get_pois_from_overpass(request,tags)
